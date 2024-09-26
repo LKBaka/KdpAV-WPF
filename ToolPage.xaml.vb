79bfb9790e8d2d618883fa5f959b8be6d50063ba
APIBox <LinkerVB@outlook.com>
2024-09-26 20:32:19 +0800

    [+ 工具箱] 新增IU Song
    新增IU Song入口

---
+++ ToolPage.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class ToolPage
+
+    Private Sub Button_Taskmgr_Click(sender As Object, e As RoutedEventArgs) Handles Button_Taskmgr.Click
+        Dim f As New TaskMgr
+        f.Show()
+    End Sub
+
+    Private Sub Button_IUSong_Click(sender As Object, e As RoutedEventArgs) Handles Button_IUSong.Click
+        Dim f As New IUSong_1
+        f.Show()
+    End Sub
+End Class
