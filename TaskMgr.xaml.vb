7c18bf9682ea0fcaf22fa80266ac3da432787f3d
APIBox <LinkerVB@outlook.com>
2024-09-26 20:33:07 +0800

    [Update] 任务管理器
    任务管理器进行部分更新

---
+++ TaskMgr.xaml.vb
@@ -0,0 +1,94 @@
+﻿Imports iNKORE.UI.WPF.Modern.Controls
+Imports System.Reflection
+
+Class TaskMgr
+    Dim WithEvents timer1 As New Forms.Timer With {
+        .Interval = 200
+    }
+    Private Async Sub KillProcess_Click(sender As Object, e As RoutedEventArgs)
+        Dim selValue = ListBox_Process.Items(ListBox_Process.SelectedIndex)
+        Debug.Print(selValue)
+        For Each p In Process.GetProcessesByName(selValue)
+            p.Kill()
+        Next
+
+        Dim msg As New ContentDialog With {
+                    .Title = "操作成功完成!",
+                    .Content = "进程 " & selValue & " 已被结束",
+                    .CloseButtonText = "确定",
+                    .DefaultButton = ContentDialogButton.Close
+            }
+        Await msg.ShowAsync
+
+    End Sub
+
+
+    Private Sub TaskMgr_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        timer1.Start()
+    End Sub
+    Private Sub RefreshProcessList()
+        Dim tmp As New ArrayList
+        For Each p In Process.GetProcesses
+            tmp.Add(p.ProcessName)
+        Next
+
+        ListBox_Process.ItemsSource = tmp
+    End Sub
+    Private Sub timer1_Tick(sender As Object, e As EventArgs) Handles timer1.Tick
+        RefreshProcessList()
+
+        'ramCounter = New PerformanceCounter("Memory", "Available MBytes")
+        'Console.WriteLine("电脑可使用内存：" + ramCounter.NextValue() + "MB")
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Refresh_Click(sender As Object, e As RoutedEventArgs)
+        RefreshProcessList()
+    End Sub
+
+    Private Sub OpenFilePath_Click(sender As Object, e As RoutedEventArgs)
+        For Each p In Process.GetProcessesByName(ListBox_Process.Items(ListBox_Process.SelectedIndex))
+            Dim procInfo As New ProcessStartInfo With {
+                .FileName = "explorer.exe",
+                .Arguments = " " & "/select," & p.MainModule.FileName
+            }
+            Dim proc As New Process With {
+                .StartInfo = procInfo
+            }
+            proc.Start()
+
+            Exit For
+        Next
+    End Sub
+
+    Private Sub Open_Click(sender As Object, e As RoutedEventArgs)
+        Dim ofd As New Windows.Forms.OpenFileDialog With {
+            .Title = "选择欲打开的文件"
+        }
+
+
+        If ofd.ShowDialog = Forms.DialogResult.OK Then
+            Process.Start(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Close_Form_Click(sender As Object, e As RoutedEventArgs)
+        Close()
+    End Sub
+
+    'Private Sub Button_CPU_Click(sender As Object, e As RoutedEventArgs) Handles Button_CPU.Click
+    '    contentframe.Navigate(New Uri("TaskMgr_性能_CPU.xaml", UriKind.Relative))
+    'End Sub
+
+    'Private Sub tabc_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles tabc.SelectionChanged
+    '    If tabc.SelectedIndex <> 1 Then
+    '        If contentframe Is Nothing Then Exit Sub
+    '        contentframe.Navigate(New Uri("BlankPage.xaml", UriKind.Relative))
+    '    End If
+    'End Sub
+End Class
---
+++ TaskMgr_性能_CPU.xaml.vb
@@ -0,0 +1,50 @@
+﻿Imports KdpAV_WPF.Win32API, System.Threading
+Class TaskMgr_性能_CPU
+    Dim WithEvents t As New Windows.Forms.Timer
+    Dim cpuCounter As New PerformanceCounter("Processor", "% Processor Time", "_Total")
+    Dim ramCounter As PerformanceCounter
+    Private Sub TaskMgr_性能_CPU_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        t.Interval = 1000
+        t.Start()
+    End Sub
+
+
+    'Public Shared Function FileTimeToInt64(ft As FILETIME) As ULong
+    '    Return (CUInt(ft.dwHighDateTime) << 32) Or ft.dwLowDateTime
+    'End Function
+
+    'Public Shared Function GetCpuUsage() As CPU_USAGE
+    '    Dim cpuUsage As New CPU_USAGE()
+    '    Dim idleTime As FILETIME, kernelTime As FILETIME, userTime As FILETIME
+
+    '    If GetSystemTimes(idleTime, kernelTime, userTime) Then
+    '        cpuUsage.idleTime = FileTimeToInt64(idleTime)
+    '        cpuUsage.kernelTime = FileTimeToInt64(kernelTime)
+    '        cpuUsage.userTime = FileTimeToInt64(userTime)
+    '    End If
+
+    '    Return cpuUsage
+    'End Function
+
+    'Public Shared Function CalculateCpuUsage(previousSample As CPU_USAGE, currentSample As CPU_USAGE, numberOfCores As Integer) As Single
+    '    Try
+    '        Dim idleDifference As ULong = currentSample.idleTime - previousSample.idleTime
+    '        Dim kernelDifference As ULong = currentSample.kernelTime - previousSample.kernelTime
+    '        Dim userDifference As ULong = currentSample.userTime - previousSample.userTime
+
+    '        Dim systemTime As ULong = kernelDifference + userDifference
+    '        Dim total As ULong = systemTime + idleDifference
+
+    '        Dim percentage As Single = CSng((100.0 * (systemTime - idleDifference) / total) / numberOfCores)
+
+    '        Return percentage
+    '    Catch ex As Exception
+
+    '    End Try
+    'End Function
+
+    Private Sub t_Tick(sender As Object, e As EventArgs) Handles t.Tick
+        ' Dim cpu = CInt(cpuCounter.NextValue()) & "%"
+
+    End Sub
+End Class
