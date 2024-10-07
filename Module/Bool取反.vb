2577c801061a0e6de38de46ce592561b4f962397
APIBox <LinkerVB@outlook.com>
2024-10-07 12:52:14 +0800

    Create Bool取反.vb

---
+++ Module/Bool取反.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Globalization
+
+Public Class BoolConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object _
+        Implements IValueConverter.Convert
+
+        If TypeOf value Is Boolean Then
+            Return Not DirectCast(value, Boolean)
+        End If
+
+        Return value
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object _
+        Implements IValueConverter.ConvertBack
+
+        If TypeOf value Is Boolean Then
+            Return Not DirectCast(value, Boolean)
+        End If
+
+        Return value
+    End Function
+End Class
