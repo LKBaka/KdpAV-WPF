16a45eb1511d8e552da2dcc6ea240e6449a7e3df
APIBox <LinkerVB@outlook.com>
2024-10-07 12:52:20 +0800

    Create RelayCommand.vb

---
+++ Module/Model/RelayCommand.vb
@@ -0,0 +1,21 @@
+﻿Public Class RelayCommand
+    Implements ICommand
+
+    Public Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
+    ReadOnly _canExecute As Func(Of Boolean)
+    ReadOnly _execute As Action
+
+    Public Sub New(argAction As Action, argCanExecute As Func(Of Boolean))
+        _canExecute = argCanExecute
+        _execute = argAction
+    End Sub
+
+    Public Function CanExecute(parameter As Object) As Boolean Implements ICommand.CanExecute
+        If _canExecute Is Nothing Then Return True
+        Return _canExecute()
+    End Function
+
+    Public Sub Execute(parameter As Object) Implements ICommand.Execute
+        _execute()
+    End Sub
+End Class
