f3d5f49b1be02f3622790ea8d881cec3dd5da80f
APIBox <LinkerVB@outlook.com>
2024-10-07 12:59:00 +0800

    Update UDiskHelper

--- U盘助手_弹窗.xaml.vb
+++ U盘助手_弹窗.xaml.vb
@@ -1,18 +1,52 @@
-﻿Public Class U盘助手_弹窗
+﻿Imports System.ComponentModel
+
+Public Class U盘助手_弹窗
+
+    Implements INotifyPropertyChanged
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
     Public DiskName As String = "" '盘符
     Public VolumeLabel = "" ' 磁盘名
     Public FreeSize As ULong = 0
+
+    Private _TitleContent = ""
+    Private _SubTitleContent = ""
+
+    Public Overridable Sub OnPropertyChanged(propertyName As String)
+        RaiseEvent PropertyChanged(Me, New System.ComponentModel.PropertyChangedEventArgs(propertyName))
+    End Sub
+    Public Property TitleContent As String
+        Get
+            Return _TitleContent
+        End Get
+        Set(value As String)
+            _TitleContent = value
+            OnPropertyChanged("TitleContent")
+        End Set
+    End Property
+    Public Property SubTitleContent As String
+        Get
+            Return _SubTitleContent
+        End Get
+        Set(value As String)
+            _SubTitleContent = value
+            OnPropertyChanged("SubTitleContent")
+        End Set
+    End Property
+
+
     Public Sub New(argDiskName As String, argVolumeLabel As String, argTotalFreeSize As ULong)
         InitializeComponent()
 
         DiskName = argDiskName
         VolumeLabel = argVolumeLabel
         FreeSize = argTotalFreeSize
+        DataContext = Me
     End Sub
 
     Private Sub U盘助手_弹窗_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-        lblTitle.Content = VolumeLabel & " [" & DiskName.Replace("\", "") & "]"
-        lblSubTitle.Content = "剩余空间:" & BytesToString(FreeSize)
+        TitleContent = VolumeLabel & " [" & DiskName.Replace("\", "") & "]"
+        SubTitleContent = "剩余空间:" & BytesToString(FreeSize)
     End Sub
 
     ' 将字节转换为GB的字符串表示
@@ -30,4 +64,5 @@
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         Process.Start("explorer", " " & DiskName) '通过explorer的启动参数跳转至磁盘
     End Sub
+
 End Class
